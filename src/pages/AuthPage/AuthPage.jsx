1f995b8ebc9519c6effacb8a6b1165b297c400ed
dekizber <dekizber@gmail.com>
2024-11-15 12:41:22 +0200

    init AuthPage

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,5 +1,13 @@
+import LoginForm from '../../components/LoginForm/LoginForm';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  return (
+    <div>
+      <LoginForm />
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default AuthPage;
